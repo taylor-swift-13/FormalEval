4898cdea94d80021eccf46f0b0489115177d582d
lx <1291729176@qq.com>
2025-12-29 22:47:57 +0800

    refactor: rename precondition and specification definitions for problem 8 for clarity

--- newspec/8.v
+++ newspec/8.v
@@ -12,8 +12,8 @@ Require Import List ZArith.
 Import ListNotations.
 Open Scope Z_scope.
 
-Definition sum_product_pre : Prop := True.
+Definition problem_8_pre : Prop := True.
 
-Definition sum_product_spec (l : list Z) (s: Z) (p : Z) : Prop :=
+Definition problem_8_spec (l : list Z) (s: Z) (p : Z) : Prop :=
     s = fold_left Z.add l 0 /\
     p = fold_left Z.mul l 1.

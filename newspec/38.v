4e017f47d3a05e0510eea7fbe90b690e45d16744
lx <1291729176@qq.com>
2025-12-30 01:58:46 +0800

    refactor: update encoding and decoding specifications for clarity and consistency

--- newspec/38.v
+++ newspec/38.v
@@ -21,45 +21,48 @@ takes as input string encoded with encode_cyclic function. Returns decoded strin
 
 
 Require Import Coq.Strings.Ascii.
-Require Import Coq.Lists.List.
+Require Import Coq.Strings.String.
 Require Import Coq.Arith.Arith.
 Require Import Coq.micromega.Lia.
 
-Import ListNotations.
+Open Scope string_scope.
 
-(* 为了使用 nth 函数，我们需要一个默认的 ascii 字符 *)
-Definition default_ascii : ascii := " "%char.
+Definition get_char (s : string) (n : nat) : ascii :=
+  match String.get n s with
+  | Some c => c
+  | None => " "%char
+  end.
 
 (* Pre: no additional constraints for `encode/decode_cyclic` by default *)
-Definition Pre (input : list ascii) : Prop := True.
+Definition problem_38_pre (input : string) : Prop := True.
 
-Definition Spec_logic (input output : list ascii) : Prop :=
+Definition problem_38_spec (input output : string) : Prop :=
   (* 1. 基础约束：长度相等 *)
-  length input = length output /\
+  String.length input = String.length output /\
   (
     (* 2. 定义常量 *)
-    let n := ((length input / 3) * 3 - 1)%nat in
-    let L := length input in
+    let n := ((String.length input / 3) * 3 - 1)%nat in
+    let L := String.length input in
 
     (* 3. 对于所有索引 i，必须满足以下由逻辑连接词构成的断言 *)
     forall i, (i < L)%nat ->
-      let out_char := nth i output default_ascii in
+      let out_char := get_char output i in
       (
         (* 情况一: i <= n *)
-        ( (i <= n)%nat /\
+        ( (i <= n)%nat ->
           (
             (* 子情况 1: (i+1)%3 = 1 *)
-            ( ((i + 1) mod 3 = 1%nat) /\ (out_char = nth (i + 2) input default_ascii) ) \/
+            ( ((i + 1) mod 3 = 1%nat) /\ (out_char = get_char input (i + 2)) ) \/
 
             (* 子情况 2: (i+1)%3 = 2 或 0 *)
-            ( (((i + 1) mod 3 = 2%nat) \/ ((i + 1) mod 3 = 0%nat)) /\ (out_char = nth (i - 1) input default_ascii) )
+            ( (((i + 1) mod 3 = 2%nat) \/ ((i + 1) mod 3 = 0%nat)) /\ (out_char = get_char input (i - 1)) )
           )
-        ) \/
+        ) /\
 
         (* 情况二: i > n (等价于 not (i <= n)) *)
-        ( (~(i <= n)%nat) /\
+        ( (~(i <= n)%nat) ->
           (
-            out_char = nth i input default_ascii
+            out_char = get_char input i
           )
         )
       )

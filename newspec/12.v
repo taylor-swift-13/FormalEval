22717bf77a054d12bba9efd242ae3a91fdebfa2e
lx <1291729176@qq.com>
2025-12-29 23:06:10 +0800

    refactor: update problem 12 specifications to use string types for consistency

--- newspec/12.v
+++ newspec/12.v
@@ -20,18 +20,20 @@ strings of the same length. Return None in case the input list is empty.
 
 Require Import Coq.Lists.List.
 Require Import Coq.Strings.Ascii.
+Require Import Coq.Strings.String.
 Import ListNotations.
+Open Scope string_scope.
 
 (* Pre: no specific requirements for `longest` — accept any list of strings by default *)
-Definition Pre (input : list (list ascii)) : Prop := True.
+Definition problem_12_pre (input : list string) : Prop := True.
 
-Definition longest_spec (input : list (list ascii)) (output : option (list ascii)) : Prop :=
+Definition problem_12_spec (input : list string) (output : option string) : Prop :=
   (input = [] /\ output = None) \/
   (exists out i,
     output = Some out /\
-    length input > 0 /\
-    i < length input /\
+    List.length input > 0 /\
+    i < List.length input /\
     nth_error input i = Some out /\
-    (forall j, j < length input -> exists s, nth_error input j = Some s -> length s <= length out) /\
-    (forall j, j < i -> exists s, nth_error input j = Some s -> length s < length out)
+    (forall j, j < List.length input -> exists s, nth_error input j = Some s -> String.length s <= String.length out) /\
+    (forall j, j < i -> exists s, nth_error input j = Some s -> String.length s < String.length out)
   ).

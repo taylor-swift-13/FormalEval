a8a12df7ea9a89474b931c30d885248aabfebf0f
lx <1291729176@qq.com>
2025-12-30 01:38:13 +0800

    refactor: reorganize polynomial evaluation definitions for clarity and consistency

--- newspec/32_.v
+++ newspec/32_.v
@@ -20,16 +20,18 @@ Require Import Coq.Lists.List.
 Require Import Coq.Arith.Arith.
 Import ListNotations.
 
-Require Import Coq.Reals.Reals.
-
 (*
- * eval_poly coeffs x 计算多项式 coeffs 在点 x 的值
+ * EvalPolyPred coeffs x res 为真，当且仅当 "res 是多项式 coeffs 在点 x 的值"
  *)
-Fixpoint eval_poly (coeffs : list R) (x : R) : R :=
-  match coeffs with
-  | [] => 0%R
-  | c :: cs => (c + x * (eval_poly cs x))%R
-  end.
+Inductive EvalPolyPred : list R -> R -> R -> Prop :=
+  | eval_poly_nil : forall x : R,
+      (* 基本情况: 空多项式的值为 0 *)
+      EvalPolyPred [] x 0
+  | eval_poly_cons : forall (c : R) (cs : list R) (x res_tail : R),
+      (* 归纳步骤: 如果已知列表尾部的评估结果... *)
+      EvalPolyPred cs x res_tail ->
+      (* ...那么可以断言整个列表的评估结果 *)
+      EvalPolyPred (c :: cs) x (c + x * res_tail).
 
 
 (* Pre: input list must be non-empty and have even length (as required by Spec) *)
@@ -37,4 +39,4 @@ Definition problem_32_pre (input : list R) : Prop := length input > 0 /\ Nat.Eve
 
 Definition problem_32_spec (input : list R) (output : R) : Prop :=
   (* 后置条件: 0 必须是多项式的一个根。*)
-  eval_poly input output = 0%R.
+  EvalPolyPred input output 0.

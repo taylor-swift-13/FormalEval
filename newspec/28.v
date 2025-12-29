ff7b547dd47f3df8d7c5d4b235cbf56798f78a19
lx <1291729176@qq.com>
2025-12-30 01:06:49 +0800

    refactor: rename Pre and Spec definitions for clarity and consistency

--- newspec/28.v
+++ newspec/28.v
@@ -12,7 +12,7 @@ Require Import List String.
 Import ListNotations.
 
 (* Pre: no additional constraints for `concatenate` by default *)
-Definition Pre (input : list string) : Prop := True.
+Definition problem_28_pre (input : list string) : Prop := True.
 
-Definition Spec (input : list string) (output : string) : Prop :=
-  fold_left String.append input EmptyString = output.
+Definition problem_28_spec (input : list string) (output : string) : Prop :=
+  String.concat "" input = output.

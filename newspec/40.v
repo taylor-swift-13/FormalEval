8b3c462d3b223b1ea73b7794c1ff1f3b7631808e
lx <1291729176@qq.com>
2025-12-30 02:03:11 +0800

    refactor: rename Pre and Spec definitions for clarity and consistency

--- newspec/40.v
+++ newspec/40.v
@@ -24,14 +24,14 @@ Require Import Coq.ZArith.ZArith.
 Import ListNotations.
 Open Scope Z_scope.
 (* Pre: no additional constraints for `triples_sum_to_zero` by default *)
-Definition Pre (input : list Z) : Prop := True.
+Definition problem_40_pre (input : list Z) : Prop := True.
 
-Definition Spec (input : list Z) (output : bool) : Prop :=
+Definition problem_40_spec (input : list Z) (output : bool) : Prop :=
   (*
     存在三个不同的索引 i, j, k，
     使得对应元素之和为 0。
     我们必须同时检查索引 i, j, k 在列表的有效范围内。
-    nth 函数需要一个默认值 (这里是0)，如果索引越界，它会返回该默认值。
+    nth 函数需要一个默认值 (这里是0)，如果索引越界，它会返回该默认值.
     但因为我们已经用 i < length input 等条件确保了索引有效，所以默认值不会被用到。
   *)
   (exists i j k : nat,

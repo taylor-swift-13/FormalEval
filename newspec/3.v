4fa442c162b75d7402c48ab807e58db3b641dcc3
lx <1291729176@qq.com>
2025-12-29 21:54:02 +0800

    refactor: rename precondition and specification definitions for clarity

--- newspec/3.v
+++ newspec/3.v
@@ -12,9 +12,9 @@ Import ListNotations.
 
 Open Scope Z_scope.
 
-Definition below_zero_pre (l : list Z) : Prop := True.
+Definition problem_3_pre (l : list Z) : Prop := True.
 (* Spec: below_zero_spec l ↔ 存在一个前缀，它的累加和小于0 *)
 
-Definition below_zero_spec (l : list Z) (output : bool): Prop :=
+Definition problem_3_spec (l : list Z) (output : bool): Prop :=
   (exists prefix suffix, prefix ++ suffix = l /\ fold_left Z.add prefix 0 < 0 <-> output = true).
 

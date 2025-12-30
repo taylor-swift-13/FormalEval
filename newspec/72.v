679c4918e8440076f8585ee70e6b9ed98e3d72f7
lx <1291729176@qq.com>
2025-12-30 16:44:28 +0800

    refactor: update problem_72_pre and problem_72_spec definitions to use Z type for consistency

--- newspec/72.v
+++ newspec/72.v
@@ -17,10 +17,12 @@ will_it_fly([3], 5) ➞ True
 # 3 is less than the maximum possible weight, and it's balanced.
 '''*)
 Require Import List.
+Require Import ZArith.
 Import ListNotations.
+Open Scope Z_scope.
 
-Definition problem_72_pre (q : list nat) (w : nat) : Prop := True.
+Definition problem_72_pre (q : list Z) (w : Z) : Prop := True.
 
 (* will_it_fly 函数的程序规约*)
-Definition  problem_72_spec (q : list nat) (w : nat) (output : bool) : Prop :=
+Definition  problem_72_spec (q : list Z) (w : Z) (output : bool) : Prop :=
   (output = true <-> (q = rev q) /\ (fold_left (fun acc x => acc + x) q 0 <= w)).

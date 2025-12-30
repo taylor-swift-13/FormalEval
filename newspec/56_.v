65e988d5539042cdd1acccecd28ae22ad7af2e2c
lx <1291729176@qq.com>
2025-12-30 14:21:29 +0800

    refactor: implement correct_bracketing function and auxiliary definitions for bracket validation

--- newspec/56_.v
+++ newspec/56_.v
@@ -18,27 +18,19 @@ Require Import Coq.Strings.String.
 Open Scope string_scope.
 
 
-Fixpoint correct_bracketing_aux (s : string) (depth : nat) : bool :=
-  match s with
-  | "" => match depth with 0 => true | _ => false end
-  | String c s' =>
-    if (Ascii.eqb c "<"%char) then
-      correct_bracketing_aux s' (S depth)
-    else if (Ascii.eqb c ">"%char) then
-      match depth with
-      | 0 => false
-      | S d => correct_bracketing_aux s' d
-      end
-    else
-      correct_bracketing_aux s' depth
-  end.
-
-Definition correct_bracketing (s : string) : bool :=
-  correct_bracketing_aux s 0.
+Inductive is_correctly_bracketed : string -> Prop :=
+  | cb_nil    : is_correctly_bracketed ""
+  | cb_nest   : forall l,
+                  is_correctly_bracketed l ->
+                  is_correctly_bracketed ("<" ++ l ++ ">")
+  | cb_concat : forall l1 l2,
+                  is_correctly_bracketed l1 ->
+                  is_correctly_bracketed l2 ->
+                  is_correctly_bracketed (l1 ++ l2).
 
 
 (* Pre: no special constraints for `correct_bracketing` *)
 Definition problem_56_pre (brackets : string) : Prop := True.
 
 Definition problem_56_spec (brackets : string) (b : bool) : Prop :=
-  b = correct_bracketing brackets.
+  b = true <-> is_correctly_bracketed brackets.

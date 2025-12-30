61a81c118a99c6d1e21e4bbd74adc5d9211d31b5
lx <1291729176@qq.com>
2025-12-30 14:29:38 +0800

    refactor: rename Pre and common_spec definitions for consistency in problem specifications

--- newspec/58.v
+++ newspec/58.v
@@ -16,9 +16,9 @@ Import ListNotations.
  * 描述了输入列表 l1, l2 与输出列表 l_out 之间的关系。
  *)
 (* Pre: no special constraints for `common` *)
-Definition Pre (l1 l2 : list Z) : Prop := True.
+Definition problem_58_pre (l1 l2 : list Z) : Prop := True.
 
-Definition common_spec (l1 l2 l_out: list Z) : Prop :=
+Definition problem_58_spec (l1 l2 l_out: list Z) : Prop :=
   (* 条件1: 公共性与完备性 *)
   (* l_out 中的任意整数 x，当且仅当 x 同时在 l1 和 l2 中。*)
   (forall x: Z, In x l_out <-> (In x l1 /\ In x l2)) /\
